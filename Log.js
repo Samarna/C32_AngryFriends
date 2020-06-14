2a9bcb1e7ba97a5d4129d0b139573cb69b92caef
Samarna <63645732+Samarna@users.noreply.github.com>
2020-06-14 12:34:56 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -1,7 +1,7 @@
 class Log extends BaseClass{
   constructor(x,y,height,angle){
     var options = { 
-      'friction' = 2.0 
+      'friction': 2.0 
     }
     super(x,y,20,height,angle);
     this.image = loadImage("sprites/wood2.png");
